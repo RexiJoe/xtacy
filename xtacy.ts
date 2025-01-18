a6e0c0f0e7a6aae0fb46784e705c9e587862f748
RexiJoe <97813374+RexiJoe@users.noreply.github.com>
2025-01-18 13:07:06 -0400

    Added Generic Types

--- xtacy.ts
+++ xtacy.ts
@@ -1,29 +1,29 @@
-export default class xtacy{
+export default class xtacy<T>{
 
-    state: any;
-    prevState: any;
+    state: any | T;
+    prevState: any | T;
     name: string;
     _target: HTMLElement;
     _event: Event;
 
-    constructor(initialState: any){
+    constructor(initialState: any | T){
         this.state = initialState;
         this.prevState = null;
         this.name = crypto.randomUUID();
         this._target = document.body;
         this._event = new Event(this.name);
     };
-    setState(newState: any){
+    setState(newState: any | T){
         this.prevState = this.state;
         this.state = newState;
         this._target.dispatchEvent(this._event);
     };
-    activateState(callback: (state: any)=> void){
+    activateState(callback: (state: any | T)=> void){
         this._target.addEventListener(this.name, ()=>{
             callback(this.state);
         });
     };
-    activatePrevState(callback: (prevState: any)=> void){
+    activatePrevState(callback: (prevState: any | T)=> void){
         this._target.addEventListener(this.name, ()=>{
             callback(this.prevState);
         });
